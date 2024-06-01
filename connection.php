ecd4b00b029743b02cfc750f8141b74069a57c07
Zeus <167625642+zeusielk@users.noreply.github.com>
2024-06-02 02:11:01 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,14 +1,10 @@
 <?php
-
 $hostname = //your host;
 $username = //database username;
 $password = //databse password;
 $dbname = //database password;
-
 $conn = new mysqli($hostname,$username,$password,$dbname);
 if($conn->connect_error){
     die("Connection error".$conn->connect_error);
 }
-
-
 ?>
